57d0dc4e64374e127ddf3fc935d2dbfd2126e837
foxstudent103311 <kolegin.yuriu@gmail.com>
2022-11-07 21:45:21 -0700

    working on class walidation form

--- js/main.js
+++ js/main.js
@@ -1,29 +1,72 @@
-const inputNme = document.querySelector('[name="Last-name"]')
-const inputEmail = document.querySelector('[type="email"]')
 
-class Validator {
-    isEmail = (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
-    isDate = (value) => !isNaN(Date.parse(value))
-    IsRequired = (htmlElem) => htmlElem.value
+const form = document.querySelector('.form')
+
+class validationFunc {
+    constructor (name, func) {
+        this.name = name
+        this.check = func 
+    }
 }
 
-class StateOfValidation extends Validator {
+class Validator {
+    constructor ([...validationFunctions]) {
+        validationFunctions.forEach((validationFunc)=> this[validationFunc.name] = validationFunc)
+    }
+}
+
+
+class StateOfValidation  {
     
     constructor(element, state = false) {
-        super()
         this.element = element
         this.state = state
+        this.typeOfValidation = element.dataset.validationType
     }
 
     #changeStateOfElement = (newState)=>this.state = newState
     #addClassFailOrSucsess = () => this.state ? this.#addSuccessStatus() : this.#addFailedStatus()
-    #addSuccessStatus = () => this.element.classList.add('success')
-    #addFailedStatus = () => this.element.classList.add('failed')
 
-    addEmailValidation = ()=>{
-        this.element.addEventListener('keydown',()=> {
-            this.#changeStateOfElement(this.isEmail(this.element.value))
+    #addSuccessStatus = () => {
+        this.element.classList.remove('failed')
+        this.element.classList.add('success')
+    }
+
+    #addFailedStatus = () => {
+        this.element.classList.remove('success')
+        this.element.classList.add('failed')
+    }
+    addValidatorToElem= ()=>{
+        const validation = this.typeOfValidation
+        const validationFn = ()=> {
+            this.#changeStateOfElement(validator[validation].check(this.element.value))
             this.#addClassFailOrSucsess()
+        }
+
+        this.element.addEventListener('keydown',validationFn)
+        this.element.addEventListener('submit',validationFn)
+    }
+}
+
+class formValidation  {
+    constructor(form) {
+        this.form = form
+
+    }
+
+    addValidationToForm = ()=>{
+        const inputs = this.form.querySelectorAll('input')
+        const inputObj = []
+        inputs.forEach((input)=>{
+            if (!input.dataset.validationType) {return}
+            const validationElem = new StateOfValidation(input)
+            validationElem.addValidatorToElem()
+            inputObj.push(validationElem)
+        })
+
+        this.form.addEventListener('submit', (e)=>{
+            e.preventDefault()
+            // console.log(inputObj.);
+            
         })
     }
 
@@ -31,7 +74,12 @@ class StateOfValidation extends Validator {
     
 }
 
-const validator = new Validator
-const validationEmail = new StateOfValidation(inputEmail)
-validationEmail.addEmailValidation()
-// inputEmail.addEventListener('click', console.log('aa'))
+const isEmail = new validationFunc ('isEmail', (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) )
+const isDate = new validationFunc('isDate',(value) => !isNaN(Date.parse(value)) )
+const isRequired = new validationFunc('isRequired', (value) => value)
+
+
+const validator = new Validator([isEmail,isDate,isRequired])
+
+const formValid = new formValidation(form)
+formValid.addValidationToForm()
