958dcddeef740bb970a62f4910ba1ef617fef68b
foxstudent103311 <kolegin.yuriu@gmail.com>
2022-12-04 15:06:34 -0700

    add class ToDO list and function add new list

--- js/main.js
+++ js/main.js
@@ -3,17 +3,47 @@ class ClickHandler {
     constructor() {
         this.windowClick = () => {
             document.addEventListener('click', (e) => {
-                var _a, _b;
+                var _a, _b, _c;
                 const target = e.target;
                 if (!(target instanceof HTMLElement)) {
                     return;
                 }
-                if (target.classList.contains('task__btn_edit')) {
-                    const id = parseInt((_b = (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.dataset.id);
-                    group.tasks[id].changingTextField();
+                const classesOfTarget = target.classList;
+                // Edit Task
+                if (classesOfTarget.contains('task__btn_edit')) {
+                    const id = (_b = (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.dataset.id;
+                    const [listNumber, taskNumber] = id.split('_');
+                    const list = toDo.plans[parseInt(listNumber)];
+                    const task = list.tasks[parseInt(taskNumber)];
+                    task.changingTextField();
+                }
+                // clear List
+                if (classesOfTarget.contains('item__clear-list')) {
+                    const id = parseInt((_c = target.parentElement) === null || _c === void 0 ? void 0 : _c.dataset.id);
+                    toDo.plans[id].clearList();
                 }
             });
         };
+        this.submitNewTaskList = () => {
+            const form = document.querySelector('#form-add-new');
+            form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) => {
+                e.preventDefault();
+                const input = form.querySelector('input');
+                let value = input.value;
+                if (value === '') {
+                    if (confirm('Create task with title : "TITLE"')) {
+                        value = 'title';
+                    }
+                    else {
+                        return;
+                    }
+                }
+                toDo.addTask(value);
+                input.value = '';
+            });
+        };
+        this.submitNewTaskList();
+        this.windowClick();
     }
 }
 class FormForEditing {
@@ -21,42 +51,42 @@ class FormForEditing {
         this._htmlElem = () => {
             const form = document.createElement('form');
             form.classList.add('item__input-block');
-            form.innerHTML = this.type === 'edit' ? `
+            form.innerHTML = this._type === 'edit' ? `
     <input type="text" class="item__input" value="${this.value}">
     <button type="submit" class="item__btn-add-note">Ok</button>
     ` : `
     <input type="text" class="item__input" placeholder="e.g. eggs">
     <button type="submit" class="item__btn-add-note">Add</button>
     `;
-            this.parentBlock.append(form);
+            this._parentBlock.append(form);
             return form;
         };
         this._addAlerts = () => {
-            document.addEventListener('click', this.alertsFN.focusInputWithAlert);
-            this.htmlElement.addEventListener('click', this.alertsFN.stopPropagation);
+            document.addEventListener('click', this._alertsFN.focusInputWithAlert);
+            this.htmlElement.addEventListener('click', this._alertsFN.stopPropagation);
         };
         this.getValue = () => {
             this.value = this.input.value;
             return this.value;
         };
         this.removeFromDOM = () => {
-            document.removeEventListener('click', this.alertsFN.focusInputWithAlert);
-            this.htmlElement.removeEventListener('click', this.alertsFN.stopPropagation);
-            this.parentBlock.removeChild(this.htmlElement);
+            document.removeEventListener('click', this._alertsFN.focusInputWithAlert);
+            this.htmlElement.removeEventListener('click', this._alertsFN.stopPropagation);
+            this._parentBlock.removeChild(this.htmlElement);
         };
-        this.type = type;
+        this._type = type;
         this.value = text;
-        this.parentBlock = parentBlock;
+        this._parentBlock = parentBlock;
         this.htmlElement = this._htmlElem();
         this.input = this.htmlElement.querySelector('input');
-        this.alertsFN = {
+        this._alertsFN = {
             stopPropagation: function (e) { e.stopPropagation(); },
             focusInputWithAlert: () => {
                 alert('Finish editing element');
                 this.input.focus();
             }
         };
-        if (this.type === 'edit') {
+        if (this._type === 'edit') {
             this._addAlerts();
         }
     }
@@ -67,7 +97,7 @@ class TaskItem {
             const task = document.createElement('li');
             task.classList.add('item__task');
             task.classList.add('task');
-            task.dataset.id = this.id.toString();
+            task.dataset.id = this.id;
             task.innerHTML = `
       <p class="task__text">${this.taskText}</p>
       <div class="task__btn-group">
@@ -78,28 +108,29 @@ class TaskItem {
             return task;
         };
         this.changingTextField = () => {
-            const taskParent = this.task.parentElement;
-            taskParent.removeChild(this.task);
+            const taskParent = this.taskHtml.parentElement;
+            taskParent.removeChild(this.taskHtml);
             const editForm = new FormForEditing(taskParent, this.taskText, 'edit');
             editForm.htmlElement.addEventListener('submit', (e) => {
                 e.preventDefault();
                 this.taskText = editForm.getValue();
                 editForm.removeFromDOM();
-                this.task = this._renderElement();
-                taskParent.append(this.task);
+                this.taskHtml = this._renderElement();
+                taskParent.append(this.taskHtml);
             });
         };
         this.taskText = text;
         this.id = id;
-        this.task = this._renderElement();
+        this.taskHtml = this._renderElement();
     }
 }
 class TaskGroup {
-    constructor() {
+    constructor(parent, title, id) {
         this._html = () => {
             const group = document.createElement('li');
+            group.dataset.id = this.id.toString();
             group.innerHTML = `
-      <h2 class="item__title">Title</h2>
+      <h2 class="item__title">${this._title}</h2>
       <div class="item__input-block"></div>
       <ul class="item__tasks-list"></ul>
       <button class="item__clear-list">Clear items</button>
@@ -109,27 +140,59 @@ class TaskGroup {
             return group;
         };
         this._addListener = () => {
-            this.inputForNewTask.htmlElement.addEventListener('submit', (e) => {
+            this._inputForNewTask.htmlElement.addEventListener('submit', (e) => {
                 e.preventDefault();
-                if (this.inputForNewTask.input.value === '') {
+                if (this._inputForNewTask.input.value === '') {
                     return alert('Cant add empty task');
                 }
-                const newTask = new TaskItem(this.inputForNewTask.input.value, this.tasks.length);
-                this.inputForNewTask.input.value = '';
+                const newTask = new TaskItem(this._inputForNewTask.input.value, this.id.toString() + '_' + this.tasks.length);
+                this._inputForNewTask.input.value = '';
                 this.tasks.push(newTask);
-                this.taskList.append(newTask.task);
+                this.taskList.append(newTask.taskHtml);
             });
         };
+        this.id = id;
+        this._parent = parent;
+        this._title = title;
         this.html = this._html();
         this.tasks = [];
-        this.inputForNewTask = new FormForEditing(this.html.querySelector('.item__input-block'), '', 'add');
+        this._inputForNewTask = new FormForEditing(this.html.querySelector('.item__input-block'), '', 'add');
         this.taskList = this.html.querySelector('.item__tasks-list');
         this._addListener();
+        this._parent.prepend(this.html);
+    }
+    clearList() {
+        this.tasks = [];
+        this.taskList.innerHTML = '';
     }
 }
-const list = document.querySelector('.todo__list');
-// const newT = new TaskItem('test',1)
+class ToDo {
+    constructor(container) {
+        this._createHtml = () => {
+            const ul = document.createElement('ul');
+            ul.classList.add('todo__list');
+            ul.innerHTML = `
+      <li class="todo__item_add-new ">
+        <h2 class="item__title">Add new tasks list</h2>
+            <form class="item__input-block" id="form-add-new"">
+              <input type="text" class="item__input" placeholder="Title">
+              <button type="submit" class="item__btn-add-note">Add</button>
+            </form>
+      </li>`;
+            this.parent.append(ul);
+            return ul;
+        };
+        this.addTask = (title) => {
+            this.plans.push(new TaskGroup(this.listHtml, title, this.plans.length));
+        };
+        this.plans = [];
+        this.parent = container;
+        this.listHtml = this._createHtml();
+    }
+}
+const container = document.querySelector('.todo__container');
+const toDo = new ToDo(container);
 const clickHandler = new ClickHandler;
-clickHandler.windowClick();
-const group = new TaskGroup();
-list === null || list === void 0 ? void 0 : list.insertAdjacentElement("afterbegin", group.html);
+toDo.addTask('title');
+toDo.addTask('title');
+toDo.addTask('title');
--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,86 @@
 class ClickHandler {
   constructor(){
+    this.submitNewTaskList()
+    this.windowClick()
   }
   windowClick = ()=>{
     document.addEventListener('click',(e)=>{
-      
       const target = e.target!
       if (!(target instanceof HTMLElement)) {return}
+      const classesOfTarget = target.classList
       
-      if (target.classList.contains('task__btn_edit')) {
-        const id = parseInt(target.parentElement?.parentElement?.dataset.id!) 
-        group.tasks[id].changingTextField()
+      // Edit Task
+      if (classesOfTarget.contains('task__btn_edit')) {
+        const id = target.parentElement?.parentElement?.dataset.id!
+        const [listNumber, taskNumber] = id.split('_')
+        const list = toDo.plans[parseInt(listNumber)]
+        const task = list.tasks[parseInt(taskNumber)]
+        task.changingTextField()
       }
+
+      // clear List
+      if (classesOfTarget.contains('item__clear-list')) {
+        const id = parseInt(target.parentElement?.dataset.id!)
+        toDo.plans[id].clearList()
+      }
+    })
+  }
+  submitNewTaskList = ()=>{
+    const form = document.querySelector('#form-add-new')
+    form?.addEventListener('submit', (e:Event)=>{
+      e.preventDefault()
+      const input = form.querySelector('input')!
+      let value = input.value
+      if (value==='') {if (confirm('Create task with title : "TITLE"')) {value='title'}else {return }}
+      toDo.addTask(value)
+      input.value = ''
     })
   }
 }
 
 class FormForEditing {
   value:string
-  type: 'edit'|'add'
+  private _type: 'edit'|'add'
   htmlElement: HTMLFormElement
   input:HTMLInputElement
-  parentBlock:HTMLElement
-  alertsFN:any
+  private _parentBlock:HTMLElement
+  private _alertsFN:any
   
 
   constructor(parentBlock:HTMLElement, text:string='',  type:'edit'|'add'='add'){
-    this.type = type
+    this._type = type
     this.value = text
-    this.parentBlock = parentBlock
+    this._parentBlock = parentBlock
     this.htmlElement = this._htmlElem()
     this.input = this.htmlElement.querySelector('input')!
-    this.alertsFN = {
+    this._alertsFN = {
       stopPropagation : function(e:Event) {e.stopPropagation()} ,
       focusInputWithAlert: ()=> {
             alert('Finish editing element')
             this.input.focus()
           }
     }
-    if (this.type==='edit') {this._addAlerts()}
+    if (this._type==='edit') {this._addAlerts()}
   }
 
   private _htmlElem = () => { 
     const form = document.createElement('form')
     form.classList.add('item__input-block')
     
-    form.innerHTML= this.type==='edit' ? `
+    form.innerHTML= this._type==='edit' ? `
     <input type="text" class="item__input" value="${this.value}">
     <button type="submit" class="item__btn-add-note">Ok</button>
     ` : `
     <input type="text" class="item__input" placeholder="e.g. eggs">
     <button type="submit" class="item__btn-add-note">Add</button>
     `
-    this.parentBlock.append(form)
+    this._parentBlock.append(form)
     return form
   }
 
   private _addAlerts = ()=>{
-    document.addEventListener('click', this.alertsFN.focusInputWithAlert)
-    this.htmlElement.addEventListener('click', this.alertsFN.stopPropagation)
+    document.addEventListener('click', this._alertsFN.focusInputWithAlert)
+    this.htmlElement.addEventListener('click', this._alertsFN.stopPropagation)
   } 
 
   getValue = ()=>{
@@ -66,27 +89,27 @@ class FormForEditing {
   }
 
   removeFromDOM = ()=> {
-    document.removeEventListener('click', this.alertsFN.focusInputWithAlert)
-    this.htmlElement.removeEventListener('click', this.alertsFN.stopPropagation)
-    this.parentBlock.removeChild(this.htmlElement)
+    document.removeEventListener('click', this._alertsFN.focusInputWithAlert)
+    this.htmlElement.removeEventListener('click', this._alertsFN.stopPropagation)
+    this._parentBlock.removeChild(this.htmlElement)
   }
 }
 
 class TaskItem {
   taskText:string
-  id:number
-  task:HTMLElement
+  id:string
+  taskHtml:HTMLElement
 
-  constructor(text:string, id:number){
+  constructor(text:string, id:string){
     this.taskText = text
     this.id = id
-    this.task = this._renderElement()
+    this.taskHtml = this._renderElement()
   }
   private _renderElement = ()=>{
     const task= document.createElement('li')
     task.classList.add('item__task')
     task.classList.add('task')
-    task.dataset.id = this.id.toString()
+    task.dataset.id = this.id
     task.innerHTML=`
       <p class="task__text">${this.taskText}</p>
       <div class="task__btn-group">
@@ -98,38 +121,47 @@ class TaskItem {
   }
 
   changingTextField = ()=>{
-    const taskParent = this.task.parentElement!
-    taskParent.removeChild(this.task)
+    const taskParent = this.taskHtml.parentElement!
+    taskParent.removeChild(this.taskHtml)
 
     const editForm = new FormForEditing(taskParent, this.taskText, 'edit')
     editForm.htmlElement.addEventListener('submit', (e:Event)=>{
       e.preventDefault()
       this.taskText = editForm.getValue()
       editForm.removeFromDOM()
-      this.task = this._renderElement()
-      taskParent.append(this.task)
+      this.taskHtml = this._renderElement()
+      taskParent.append(this.taskHtml)
     })
   }
 }
 
 class TaskGroup {
+  id:number
   tasks:TaskItem[]
   html:HTMLElement
-  inputForNewTask:FormForEditing
+  private _inputForNewTask:FormForEditing
   taskList:HTMLUListElement
+  private _parent:Element
+  private _title:string
 
-  constructor(){
+  constructor(parent:Element, title:string, id:number){
+    this.id = id 
+    this._parent = parent
+    this._title = title
     this.html = this._html()
     this.tasks = []
-    this.inputForNewTask = new FormForEditing (this.html.querySelector('.item__input-block')!,'','add')
+    this._inputForNewTask = new FormForEditing (this.html.querySelector('.item__input-block')!,'','add')
     this.taskList = this.html.querySelector('.item__tasks-list')!
     this._addListener()
+    this._parent.prepend(this.html)
   }
 
   private _html = ()=>{
     const group = document.createElement('li')
+    
+    group.dataset.id = this.id.toString()
     group.innerHTML=`
-      <h2 class="item__title">Title</h2>
+      <h2 class="item__title">${this._title}</h2>
       <div class="item__input-block"></div>
       <ul class="item__tasks-list"></ul>
       <button class="item__clear-list">Clear items</button>
@@ -140,24 +172,57 @@ class TaskGroup {
   }
 
   private _addListener = ()=>{
-    this.inputForNewTask.htmlElement.addEventListener('submit', (e:Event)=>{
+    this._inputForNewTask.htmlElement.addEventListener('submit', (e:Event)=>{
       e.preventDefault()
-      if (this.inputForNewTask.input.value==='') { return alert('Cant add empty task') }
-      const newTask = new TaskItem(this.inputForNewTask.input.value,this.tasks.length)
-      this.inputForNewTask.input.value = ''
+      if (this._inputForNewTask.input.value==='') { return alert('Cant add empty task') }
+      const newTask = new TaskItem(this._inputForNewTask.input.value,this.id.toString()+'_'+this.tasks.length)
+      this._inputForNewTask.input.value = ''
       this.tasks.push(newTask)
-      this.taskList.append(newTask.task)
+      this.taskList.append(newTask.taskHtml)
     })
   }
 
+  public clearList() {
+    this.tasks = []
+    this.taskList.innerHTML = ''
+  }
+
 }
-const list = document.querySelector('.todo__list')
-// const newT = new TaskItem('test',1)
 
+class ToDo {
+  listHtml:HTMLElement
+  plans:TaskGroup[]
+  parent:Element
+  constructor(container:Element) {
+    this.plans = []
+    this.parent = container
+    this.listHtml = this._createHtml()
 
+  }
+  private _createHtml = ()=>{
+    const ul = document.createElement('ul')
+    ul.classList.add('todo__list')
+    ul.innerHTML=`
+      <li class="todo__item_add-new ">
+        <h2 class="item__title">Add new tasks list</h2>
+            <form class="item__input-block" id="form-add-new"">
+              <input type="text" class="item__input" placeholder="Title">
+              <button type="submit" class="item__btn-add-note">Add</button>
+            </form>
+      </li>`
+    this.parent.append(ul)
+    return ul
+  }
+  public addTask = (title:string)=>{
+    this.plans.push(new TaskGroup(this.listHtml, title,this.plans.length))
+  }
+}
+const container = document.querySelector('.todo__container')!
+const toDo = new ToDo(container)
 const clickHandler = new ClickHandler
-clickHandler.windowClick()
 
-const group = new TaskGroup()
-list?.insertAdjacentElement("afterbegin",group.html)
+toDo.addTask('title')
+toDo.addTask('title')
+toDo.addTask('title')
+
 
