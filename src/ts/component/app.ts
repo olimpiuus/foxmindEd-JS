aae710dc44528c914972873213eb30a40931bcf0
foxstudent103311 <kolegin.yuriu@gmail.com>
2023-01-05 21:51:19 -0700

    finish render class / add filter method for counting quantity elements

--- src/ts/component/app.ts
+++ src/ts/component/app.ts
@@ -1,83 +1,13 @@
-import dataShop, { IMinMaxObj, IProduct, ShopData } from './getShopData';
 
-console.log(dataShop);
+import { getDataArr, ShopData } from './getShopData';
+import { Render } from './render';
 
-class Render {
-  list: HTMLUListElement;
-  sidebar: HTMLElement;
 
-  constructor() {
-    this.list = document.querySelector('.products')!;
-    this.sidebar = document.querySelector('.sidebar')!;
-  }
 
-  renderHtmlProductsList = (productArray: IProduct[]) => {
-    this.list.innerHTML = '';
-    productArray.forEach((elem) => {
-      const { title, price, images } = elem;
-      this.list.insertAdjacentHTML(
-        'beforeend',
-        `
-      <li class="products__item shopping-item">
-        <img src="${images[0]}" alt="" class="shopping-item__img">
-        <h3 class="shopping-item__name">${title}</h3>
-        <data class="shopping-item__price-value" value="${price}">$${price}</data>
-      </li>
-    `
-      );
-    });
-  };
-
-  private _renderSearch = ()=>{
-    this.sidebar.insertAdjacentHTML('afterbegin', `
-    <input class="sidebar__search" type="text" name="search" placeholder="Search..." />
-    `)
-  }
-
-  private _renderFilterCategory = (name:string, values:string[])=>{
-    this.sidebar.insertAdjacentHTML(
-      'beforeend',
-      `
-      <h3 class="sidebar__title">${name}</h3>
-      <ul class="sidebar__filter-list" id="categoriesList${name}">
-        <li class="sidebar__filter-item">All</li>
-      </ul>
-      `
-    );
-    const listFilter = this.sidebar.querySelector(`#categoriesList${name}`);
-    values.forEach((value) => {
-      listFilter?.insertAdjacentHTML(
-        'beforeend',
-        `
-      <li class="sidebar__filter-item">${value}</li>`
-      );
-    });
-  }
-
-  private _renderPriceRange = (arr:IMinMaxObj)=>{
-    this.sidebar.insertAdjacentHTML(
-      'beforeend',
-      `
-      <h3 class="sidebar__title">Price</h3>
-      <input class="sidebar__price-range" type="range" name="price-range" max="${arr.max}" min="${arr.min}" />
-      <data class="sidebar__filter-max-price">Value : $80</data>
-      `
-    );
-  }
-
-  renderSideBar = (data: ShopData) => {
-    this._renderSearch()
-
-    Object.entries(data.filters).forEach((prop) => {
-      const [name, value] = prop;
-      this._renderFilterCategory(name,value)
-    });
-    this._renderPriceRange(data.priceRange)
-  };
-}
 
 (async () => {
-  await dataShop.initialize();
-  new Render().renderHtmlProductsList(dataShop.list!);
-  new Render().renderSideBar(dataShop);
+  const data = new ShopData(await getDataArr());
+  new Render().renderHtmlProductsList(data.list!);
+  new Render().renderSideBar(data);
+
 })();
--- src/ts/component/getShopData.ts
+++ src/ts/component/getShopData.ts
@@ -1,3 +1,5 @@
+import { ICollectionWithCount,countQuantitiesUniqEntries } from "./countQuantitiesUniqEntries";
+
 export interface IProduct {
   id: number;
   title: string;
@@ -13,57 +15,66 @@ export interface IProduct {
 }
 
 export interface IMinMaxObj {
-  min:number,
-  max:number
+  min: number;
+  max: number;
 }
 
 interface IAnswerServer {
   products: IProduct[];
 }
 
+export const getDataArr = async () => {
+  try {
+    const answer = await fetch('https://dummyjson.com/products?limit=100');
+    const data: IAnswerServer = await answer.json();
+    return data.products;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export class ShopData {
-  list: IProduct[] | undefined;
-  filters!: {
-    brands: string[];
-    categories: string[];
+  filters: {
+    categories: ICollectionWithCount;
+    brands: ICollectionWithCount;
   };
-  priceRange!: IMinMaxObj
+  priceRange!: IMinMaxObj;
+  filteredList: IProduct[] 
 
-  private _requestFromServ = async () => {
-    try {
-      const answer = await fetch('https://dummyjson.com/products?limit=100');
-      const data: IAnswerServer = await answer.json();
-      return data.products;
-    } catch (error) {
-      console.log(error);
+  constructor(public list: IProduct[] | undefined) {
+    this.filters = {
+      categories: {},
+      brands: {},
+    };
+    this.filteredList = this.list!
+    
+    if (list) {
+      this.filters.brands = this._getBrands();
+      this.filters.categories = this._getCategories();
+      this.priceRange = this._getPriceRange()!;
+      (()=>{
+        console.log(new Set(this.list?.map((product) => product.brand)).values());
+        
+      })()
+      
     }
-  };
+  }
 
-  private _getBrands = () => [...new Set(this.list?.map((product) => product.brand))];
+  private _getBrands = () => countQuantitiesUniqEntries(this.list?.map(product => product.brand)!);
 
-  private _getCategories = () => [...new Set(this.list?.map((product) => product.category))];
+  private _getCategories = () => countQuantitiesUniqEntries(this.list?.map((product) => product.category)!);
 
   private _getPriceRange = () => {
     const prices = this.list?.map((product) => product.price);
     if (prices) {
       return {
-        min:Math.min(...prices),
+        min: Math.min(...prices),
         max: Math.max(...prices)
       };
     }
   };
-
-  public initialize = async () => {
-    this.list = await this._requestFromServ();
-    this.filters = {
-      brands: [],
-      categories: []
-    };
-    this.filters.brands = this._getBrands();
-    this.filters.categories = this._getCategories();
-    this.priceRange = this._getPriceRange()!;
-  };
 }
-const dataShop = new ShopData();
+function collectionWithCount(arg0: (string[] | undefined)[]) {
+  throw new Error("Function not implemented.");
+}
 
-export default dataShop;
